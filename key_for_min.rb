32c62f57e7bcd48416542afa16b56f8028f0dc5d
powell956 <powell956@gmail.com>
2018-12-31 04:16:52 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash == {}
+    return nil
+  end
+
+  smallest = (2**(0.size * 8 -2) -1)
+  smallest_name = ""
+
+  name_hash.each do |name, num|
+    if num < smallest
+      smallest = num
+      smallest_name = name
+    end
+  end
+
+  smallest_name
 end
